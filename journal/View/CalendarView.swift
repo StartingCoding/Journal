c0582492d39d93c46d17ea0bbe9657a0f182b222
StartingCoding <StartingCodingPartner@gmail.com>
2021-02-22 11:50:31 +0100

    Created Row View for Month

--- journal/View/CalendarView.swift
+++ journal/View/CalendarView.swift
@@ -26,30 +26,34 @@ struct CalendarView: View {
     }
 }
 
+
+// MARK: List
 struct CalendarListView: View {
     var calendarContent: [CalModel]
     
-    @State private var yearsBool = false
+    @State private var showingYears = false
     
     var body: some View {
         Button(action: {
-            yearsBool.toggle()
+            showingYears.toggle()
         }, label: {
             HStack {
                 Text("5 Years")
                     .foregroundColor(Color.primary)
                 Spacer()
-                Image(systemName: yearsBool ? "chevron.down" : "chevron.right")
+                Image(systemName: showingYears ? "chevron.down" : "chevron.right")
             }
             .padding()
         })
         
-        if yearsBool {
-            CalendarYearRowView(calendarContent: calendarContent, yearsBool: yearsBool)
+        if showingYears {
+            CalendarYearRowView(calendarContent: calendarContent, yearsBool: showingYears)
         }
     }
 }
 
+
+// MARK: Year Row
 struct CalendarYearRowView: View {
     var calendarContent: [CalModel]
     var yearsBool: Bool
@@ -71,7 +75,7 @@ struct CalendarYearRowView: View {
             })
             
             if yearBool[index] {
-                CalendarMonthRowView(year: calendarContent[index])
+                CalendarMonthRowView(year: calendarContent[index], showingMonth: yearBool[index])
             }
         }
     }
@@ -84,20 +88,56 @@ struct CalendarYearRowView: View {
     }
 }
 
+
+// MARK: Month Row
 struct CalendarMonthRowView: View {
     var year: CalModel
-    var months: [CalModel] {
-        if let months = year.subCalModel {
-            return months
-        } else {
-            fatalError("🔴 Months not found in a specific year")
+    var months: [CalModel]
+    
+    @State private var showingDays: [Bool]
+    var showingMonth: Bool
+    
+    var body: some View {
+        ForEach(0..<months.count) { index in
+            Button(action: {
+                showingDays[index].toggle()
+            }, label: {
+                HStack {
+                    Text(months[index].name)
+                        .foregroundColor(Color.primary)
+                    Spacer()
+                    Image(systemName: showingMonth ? "chevron.down" : "chevron.right")
+                }
+                .padding()
+            })
+            
+            if showingDays[index] {
+                Text("Success!")
+            }
         }
     }
     
-    var body: some View {
-        ForEach(months, id: \.name) { month in
-            Text("\(month.name)")
+    init(year: CalModel, showingMonth: Bool) {
+        self.year = year
+        
+        if let months = self.year.subCalModel {
+            self.months = months
+        } else {
+            fatalError("🔴 Months not found in a specific year")
         }
+        
+        self._showingDays = State(initialValue: Array(repeating: false, count: months.count))
+        self.showingMonth = showingMonth
+    }
+}
+
+
+// MARK: Day Row
+struct CalendarDayRowView: View {
+    var month: CalModel
+    
+    var body: some View {
+        Text("\(month.name)")
     }
 }
 
